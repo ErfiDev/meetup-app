feab54548ab7bfdc7b47115040c579df41980ba5
Erfan <erfanhanifezade@gmail.com>
2021-07-19 11:46:00 +0430

    delete practice files

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,7 @@ import Link from "next/link";
 const HomePage = ()=> {
     return (
         <>
-            <title>hello</title>
-            <Link href="/news">
-                link to news
-            </Link><br />
-            <Link href={`/news/salam`}>
-                dynamic route
-            </Link><br />
-            <Link href={"/news/something"}>
-                Something
-            </Link><br />
-            <Link href="/serverSide">Server Side Props</Link>
-            <br />
-            <Link href={"/swr"}>
-                SWR
-            </Link>
+            salam
         </>
     )
 }
