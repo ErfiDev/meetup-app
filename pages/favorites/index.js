34c525d4bdb35e49b77fd2d7e6430a92798e0a6b
Erfan <erfanhanifezade@gmail.com>
2021-08-06 02:02:00 +0430

    create favorites page

---
+++ pages/favorites/index.js
@@ -0,0 +1,15 @@
+import Layout from "../../components/layouts/layout";
+import Head from "next/head";
+
+const Favorites = () => {
+  return (
+    <Layout>
+      <Head>
+        <title>Favorites</title>
+      </Head>
+      <section>this</section>
+    </Layout>
+  );
+};
+
+export default Favorites;
