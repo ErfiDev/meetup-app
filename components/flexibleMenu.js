564f5cb220ffdd734cce47966be88725e9f9c7eb
Erfan <erfanhanifezade@gmail.com>
2021-08-14 02:09:11 +0430

    side bar code complete

--- components/flexibleMenu.js
+++ components/flexibleMenu.js
@@ -1,23 +1,29 @@
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import Image from "next/image";
 
-export const FlexsibleMenu = () => {
-  const toggle = useSelector((state) => state.toggleManu);
-
-  return (
-    <div
-      className="flexible-menu"
-      style={{ display: toggle ? "flex" : "none" }}
-    >
-      flexuble menu
-    </div>
-  );
-};
-
 export const FlexibleMenuButton = () => {
+  const dis = useDispatch();
+  const toggle = useSelector((state) => state.Toggle);
+  function changeToggle() {
+    if (toggle) {
+      dis({ type: "FALSE_TOGGLE" });
+    } else {
+      dis({ type: "TRUE_TOGGLE" });
+    }
+  }
   return (
-    <div className="flexible-menu-button">
-      <Image src="/hamburger.png" alt="hamburger" width="50px" height="50px" />
-    </div>
+    <>
+      <div className="flexible-menu-button" onClick={changeToggle}>
+        <Image
+          src="/hamburger.png"
+          alt="hamburger"
+          width="50px"
+          height="50px"
+        />
+      </div>
+      <div className="flexible-menu" style={{ left: toggle ? "0" : "-100vw" }}>
+        flexuble menu
+      </div>
+    </>
   );
 };
