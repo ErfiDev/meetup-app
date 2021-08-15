1894b231fa28f2c8a84b2d7a27e2af83f03deb7b
Erfan <erfanhanifezade@gmail.com>
2021-08-16 00:04:30 +0430

    complete flexible menu

--- components/flexibleMenu.js
+++ components/flexibleMenu.js
@@ -1,9 +1,14 @@
 import { useSelector, useDispatch } from "react-redux";
 import Image from "next/image";
+import { useRouter } from "next/router";
+import Link from "next/link";
 
 export const FlexibleMenuButton = () => {
   const dis = useDispatch();
   const toggle = useSelector((state) => state.Toggle);
+  const router = useRouter();
+  let { asPath } = router;
+
   function changeToggle() {
     if (toggle) {
       dis({ type: "FALSE_TOGGLE" });
@@ -11,6 +16,13 @@ export const FlexibleMenuButton = () => {
       dis({ type: "TRUE_TOGGLE" });
     }
   }
+
+  function toFalse() {
+    return dis({
+      type: "FALSE_TOGGLE",
+    });
+  }
+
   return (
     <>
       <div className="flexible-menu-button" onClick={changeToggle}>
@@ -21,8 +33,48 @@ export const FlexibleMenuButton = () => {
           height="50px"
         />
       </div>
-      <div className="flexible-menu" style={{ left: toggle ? "0" : "-100vw" }}>
-        flexuble menu
+      <div
+        className="flexible-menu flex justify-center items-center"
+        style={{ left: toggle ? "0" : "-100vw" }}
+      >
+        <ul className="m-0 p-0 flex flex-col justify-center items-center">
+          <li
+            className={
+              asPath === "/"
+                ? "selected m-5 text-base antialiased text-white cursor-pointer header-list-options"
+                : "m-5 text-base antialiased text-white cursor-pointer header-list-options"
+            }
+            onClick={toFalse}
+          >
+            <Link href="/">All meetups</Link>
+          </li>
+          <li
+            className={
+              asPath === "/addNew"
+                ? "selected m-5 text-base antialiased text-white cursor-pointer header-list-options"
+                : "m-5 text-base antialiased text-white cursor-pointer header-list-options"
+            }
+            onClick={toFalse}
+          >
+            <Link href="/addNew">add new meetup</Link>
+          </li>
+          <li
+            className={
+              asPath === "/favorites"
+                ? "selected m-5 text-base antialiased text-white cursor-pointer header-list-options"
+                : "m-5 text-base antialiased text-white cursor-pointer header-list-options"
+            }
+            onClick={toFalse}
+          >
+            <Link href="/favorites">my favorites</Link>
+          </li>
+          <li
+            onClick={toFalse}
+            className="flexible-menu-signin text-base antialiased text-white cursor-pointer header-list-options"
+          >
+            <Link href="/account">sign in</Link>
+          </li>
+        </ul>
       </div>
     </>
   );
