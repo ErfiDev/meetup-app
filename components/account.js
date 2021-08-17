48292ac3bef49690ccfa3b838d18eaa246b2ac2c
Erfan <erfanhanifezade@gmail.com>
2021-08-17 20:44:27 +0430

    fix input type password

--- components/account.js
+++ components/account.js
@@ -52,7 +52,7 @@ export const SignIn = ({ formHandler }) => {
             <input
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="password"
-              type="text"
+              type="password"
               placeholder="Password"
               onChange={onChange("password")}
               value={data.password}
@@ -163,7 +163,7 @@ export const Register = ({ formHandler }) => {
             <input
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="password"
-              type="text"
+              type="password"
               placeholder="Password"
               onChange={onChange("password")}
               value={data.password}
