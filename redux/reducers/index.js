ff0835f45221a243cd6b455e0c34ebbc8996017c
Erfan <erfanhanifezade@gmail.com>
2021-08-22 22:22:04 +0430

    create user reducer

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import Toggle from "./toggle";
+import User from "./user";
 
 const Reducers = combineReducers({
   Toggle,
+  User,
 });
 
 export default Reducers;
