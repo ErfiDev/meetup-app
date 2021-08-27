b0166fc763baa2c6d2a9852339aca80f166cbf65
Erfan <erfanhanifezade@gmail.com>
2021-08-27 19:37:14 +0430

    add title for meetup single page

--- pages/meetup/[key].js
+++ pages/meetup/[key].js
@@ -3,6 +3,7 @@ import { useEffect } from "react";
 import nProgress from "nprogress";
 import Image from "next/image";
 import { getMeetUps } from "services/meetup";
+import Head from "next/head";
 
 const MeetUp = ({ meetup }) => {
   const router = useRouter();
@@ -18,33 +19,38 @@ const MeetUp = ({ meetup }) => {
   }, []);
 
   return (
-    <section
-      key={key}
-      className="w-full min-h-half flex flex-row justify-around items-center flex-wrap"
-    >
-      <Image
-        src={meetup.image ? meetup.image : "/picture.png"}
-        alt={meetup.name}
-        width="450px"
-        height="300px"
-        className="object-cover rounded cursor-pointer"
-      />
-      <div>
-        <h1 className="mb-10 text-4xl">{meetup.name}</h1>
-        <address className="mb-5">{meetup.address}</address>
-        <button className="btn-signin-meetup p-2 pr-6 pl-6 text-white mt-20 text-2xl rounded-full bg-purple-700 transition-all">
-          sign in
-        </button>
-      </div>
-      <div className="backlink-container mt-10 w-full flex justify-center items-center min-h-forBackLinks">
-        <button
-          className="bg-none transition-all outline-none border-none text-2xl"
-          onClick={() => router.back()}
-        >
-          Back
-        </button>
-      </div>
-    </section>
+    <>
+      <Head>
+        <title>{meetup.name}</title>
+      </Head>
+      <section
+        key={key}
+        className="w-full min-h-half flex flex-row justify-around items-center flex-wrap"
+      >
+        <Image
+          src={meetup.image ? meetup.image : "/picture.png"}
+          alt={meetup.name}
+          width="450px"
+          height="300px"
+          className="object-cover rounded cursor-pointer"
+        />
+        <div>
+          <h1 className="mb-10 text-4xl">{meetup.name}</h1>
+          <address className="mb-5">{meetup.address}</address>
+          <button className="btn-signin-meetup p-2 pr-6 pl-6 text-white mt-20 text-2xl rounded-full bg-purple-700 transition-all">
+            sign in
+          </button>
+        </div>
+        <div className="backlink-container mt-10 w-full flex justify-center items-center min-h-forBackLinks">
+          <button
+            className="bg-none transition-all outline-none border-none text-2xl"
+            onClick={() => router.back()}
+          >
+            Back
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
