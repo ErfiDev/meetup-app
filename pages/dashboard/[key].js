8b250b56fcc2c7c04791168c18555b0b26d86bb1
Erfan <erfanhanifezade@gmail.com>
2021-08-29 15:28:11 +0430

    fix bug changePass page

--- pages/dashboard/[key].js
+++ pages/dashboard/[key].js
@@ -84,7 +84,7 @@ export default function ChangePass({ user }) {
               className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
               id="newPass"
               type="text"
-              placeholder="newPass"
+              placeholder="New Password"
               onChange={onChange("newPass")}
               value={data.newPass}
             />
